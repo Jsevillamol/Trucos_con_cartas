1fb24e69827f6bc5fdffa9a6b82d9d100a399cb9
Jsevillamol <jsevillamol@outlook.com>
2014-12-16 20:31:47 +0100

    Implementado cargar()

--- Trucos_con_cartas.cpp
+++ Trucos_con_cartas.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -26,7 +28,39 @@ typedef tCarta tMazo[MAX_CARTAS+1];
 
 //FUNCIONES
 
+bool cargar(tMazo mazo, string &nomb);
+bool abrir(string &nomb, ifstream &archivo);
+string traducir(tCarta carta);
+tCarta traducir(string carta);
+
 int main()
 {
 
 }
+
+//Carga un mazo de un archivo a elección del usuario.
+bool cargar(tMazo mazo, string &nomb)
+{
+	string linea;
+	int cont=0;
+	ifstream archivo;
+	if (abrir(nomb, archivo))
+	{
+		getline(linea, archivo);
+		while (linea != "x" && cont < MAX_CARTAS)
+		{
+			mazo[cont] = traducir(linea);
+			getline(linea, archivo);
+			cont++;
+		}
+		mazo[cont] = CENTINELA;
+		return true;
+	}
+	else return false;
+}
+
+//Abre el archivo que indica el usuario. Si el archivo no existe, se pregunta de nuevo, hasta tres veces.
+bool abrir(string &nomb, ifstream &archivo);
+
+string traducir(tCarta carta);
+tCarta traducir(string carta);
