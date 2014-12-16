cc3e8fdd05c18f35b0130002e39bb0e47facf18a
Jsevillamol <jsevillamol@outlook.com>
2014-12-16 20:48:56 +0100

    Implementado barajar()
    Acabo de darme cuenta de que la estoy fastidiando un poco al trabajar
    directamente sobre master. Intentaré volver a una versión compilable
    ipso facto.

--- Trucos_con_cartas.cpp
+++ Trucos_con_cartas.cpp
@@ -27,11 +27,16 @@ typedef int tCarta;
 typedef tCarta tMazo[MAX_CARTAS+1];
 
 //FUNCIONES
+int cuantas(tMazo mazo);
 
 bool cargar(tMazo mazo, string &nomb);
 bool abrir(string &nomb, ifstream &archivo);
 string traducir(tCarta carta);
 tCarta traducir(string carta);
+
+void barajar(tMazo mazo);
+void intercambiar (tMazo mazo, int posicion1, int posicion2);
+int randint(int max);
 
 int main()
 {
@@ -64,3 +69,16 @@ bool abrir(string &nomb, ifstream &archivo);
 
 string traducir(tCarta carta);
 tCarta traducir(string carta);
+
+
+//Baraja el mazo, intercambiando aleatoriemente cartas
+void barajar(tMazo mazo)
+{
+	int nCartas = cuantas(mazo), pos1, pos2;
+	for (int i=0; i<3*nCartas; i++)
+	{
+		pos1 = randint(nCartas);
+		pos2 = randint(nCartas);
+		intercambiar(mazo, pos1, pos2);
+	}
+}
